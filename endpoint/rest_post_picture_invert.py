2703c5c4369121e6dea3eb136d5c6a48a572fa2d
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-13 10:06:37 +0100

    dodano: funkcję zwracającą przesłany obraz oraz formularz przesyłania

--- endpoint/rest_post_picture_invert.py
+++ endpoint/rest_post_picture_invert.py
@@ -17,12 +17,30 @@ class RestPostPictureInvert(tornado.web.RequestHandler):
         """
         return None
 
+    def get(self) -> None:
+        """
+        Zwrócenie prostego formularza do przesyłania obrazów do endpoint.
+
+        :return: None
+        """
+        logging.info("[HTTP GET] / 200")
+        self.write("<html>" +
+                   "<form method='post' enctype='multipart/form-data'>" +
+                   "<input name='obraz' type='file' accept='*.jpg' />" +
+                   "<br/>" +
+                   "<br/>" +
+                   "<input type='submit' />" +
+                   "</form>" +
+                   "</html>")
+        return None
+
     def post(self) -> None:
         """
         Zwrócenie przesłanego obrazu po wykonaniu na nim polecenia inwersji kolorów.
 
         :return: None
         """
-        logging.info("[HTTP GET] / 200")
-        self.write({})
+        logging.info("[HTTP POST] / 200")
+        self.set_header("Content-type", self.request.files['obraz'][0]['content_type'])
+        self.write(self.request.files['obraz'][0]['body'])
         return None
