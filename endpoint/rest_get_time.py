23e1681c46a3538431dc43585747b7cf54532ead
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-26 09:25:49 +0100

    dodano: endpoint odpowiedzialny za zwracanie aktualnego czasu

---
+++ endpoint/rest_get_time.py
@@ -0,0 +1,29 @@
+import logging
+import tornado.web
+
+from typing import Optional, Awaitable
+from datetime import datetime
+
+
+class RestGetTime(tornado.web.RequestHandler):
+    """
+    Klasa odpowiadająca za endpoint zwracający aktualną godzinę po zalogowaniu.
+    """
+    def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
+        """
+        Obsługa zdarzenia przesłania danych do przeglądarki.
+
+        :param chunk: wielkość przesłanej paczki danych
+        :return: None
+        """
+        return None
+
+    def get(self) -> None:
+        """
+        Zwrócenie aktualnej godziny zalogowanemu użytkownikowi.
+
+        :return: None
+        """
+        logging.info("[HTTP GET] /time 200")
+        self.write({"time": datetime.now().strftime("%H:%M:%S")})
+        return None
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tornado.web
 from endpoint.rest_get_root import RestGetRoot
 from endpoint.rest_get_prime import RestGetPrime
 from endpoint.rest_post_picture_invert import RestPostPictureInvert
+from endpoint.rest_get_time import RestGetTime
 
 
 def make_app():
@@ -13,7 +14,8 @@ def make_app():
         [
             (r"/", RestGetRoot),
             (r"/prime/([0-9]+)", RestGetPrime),
-            (r"/picture/invert", RestPostPictureInvert)
+            (r"/picture/invert", RestPostPictureInvert),
+            (r"/time", RestGetTime)
         ],
         debug=False
     )
