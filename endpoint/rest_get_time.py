30f4ab481de9463f7e0e681424bdfb36f8c98900
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-26 10:40:21 +0100

    dodano: uwierzytelnianie BASIC AUTH do endpointu z czasem

--- endpoint/rest_get_time.py
+++ endpoint/rest_get_time.py
@@ -1,12 +1,10 @@
 import os
 import logging
 import tornado.web
+import base64
 
 from typing import Optional, Awaitable
 from datetime import datetime
-
-API_AUTH_USER = os.getenv('API_AUTH_USER')
-API_AUTH_PASS = os.getenv('API_AUTH_PASS')
 
 
 class RestGetTime(tornado.web.RequestHandler):
@@ -29,5 +27,29 @@ class RestGetTime(tornado.web.RequestHandler):
         :return: None
         """
         logging.info("[HTTP GET] /time 200")
+
+        auth_data: str = self.request.headers.get("Authorization")
+
+        if auth_data is None or (auth_data is not None and "Basic " not in auth_data):
+            self.set_status(401)
+            self.set_header("WWW-Authenticate", "Basic realm=\"Restricted\"")
+            self.finish()
+            return None
+
+        api_auth_user = os.getenv('API_AUTH_USER')
+        api_auth_pass = os.getenv('API_AUTH_PASS')
+
+        auth_string: str = api_auth_user + ":" + api_auth_pass
+        auth_bytes: bytes = auth_string.encode("ascii")
+        base64_bytes: bytes = base64.b64encode(auth_bytes)
+        base64_string: str = base64_bytes.decode("ascii")
+        auth_data: str = auth_data.split(" ")[1]
+
+        if auth_data != base64_string:
+            self.set_status(401)
+            self.set_header("WWW-Authenticate", "Basic realm=\"Restricted\"")
+            self.finish()
+            return None
+
         self.write({"time": datetime.now().strftime("%H:%M:%S")})
         return None
