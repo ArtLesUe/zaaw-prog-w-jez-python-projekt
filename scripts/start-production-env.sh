b18654cb1388bdc03355a013e90741c5c2b85022
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-11-30 17:31:29 +0100

    dodano: automatyczny restart kontenera w przypadku crashu

--- scripts/start-production-env.sh
+++ scripts/start-production-env.sh
@@ -10,8 +10,8 @@ docker rm arturapi-node-3
 docker rm arturapi-node-4
 docker rm arturapi-balancer
 
-docker run --publish 8888:8888 --name arturapi-balancer --detach arturapi:v2
-docker run --publish 8889:8888 --name arturapi-node-1 --detach arturapi:v1
-docker run --publish 8890:8888 --name arturapi-node-2 --detach arturapi:v1
-docker run --publish 8891:8888 --name arturapi-node-3 --detach arturapi:v1
-docker run --publish 8892:8888 --name arturapi-node-4 --detach arturapi:v1
+docker run --restart unless-stopped --publish 8888:8888 --name arturapi-balancer --detach arturapi:v2
+docker run --restart unless-stopped --publish 8889:8888 --name arturapi-node-1 --detach arturapi:v1
+docker run --restart unless-stopped --publish 8890:8888 --name arturapi-node-2 --detach arturapi:v1
+docker run --restart unless-stopped --publish 8891:8888 --name arturapi-node-3 --detach arturapi:v1
+docker run --restart unless-stopped --publish 8892:8888 --name arturapi-node-4 --detach arturapi:v1
